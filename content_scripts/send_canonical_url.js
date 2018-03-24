11caf3806ddcd3a4f605457c26a087433e8d26fc
yukihane <yukihane.feather@gmail.com>
2018-03-24 11:44:58 +0900

    デバッグログ出力削除

--- content_scripts/send_canonical_url.js
+++ content_scripts/send_canonical_url.js
@@ -1,5 +1,4 @@
 const canoUrl = document.querySelector('link[rel="canonical"]');
 
 const target = canoUrl ? canoUrl.href : null;
-console.log("canoUrl: " + target);
 browser.runtime.sendMessage({"url": target});
