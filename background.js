872e7264246f060978e1c57a4658f8a9b59f66d9
yukihane <yukihane.feather@gmail.com>
2021-10-15 11:04:37 +0900

    prettierで整形

--- background.js
+++ background.js
@@ -14,7 +14,10 @@ var CATGIFS = "http://chilloutandwatchsomecatgifs.com/";
 /*
 Restart alarm for the currently active tab, whenever background.js is run.
 */
-var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
+var gettingActiveTab = browser.tabs.query({
+  active: true,
+  currentWindow: true,
+});
 gettingActiveTab.then((tabs) => {
   restartAlarm(tabs[0].id);
 });
@@ -26,7 +29,10 @@ browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (!changeInfo.url) {
     return;
   }
-  var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
+  var gettingActiveTab = browser.tabs.query({
+    active: true,
+    currentWindow: true,
+  });
   gettingActiveTab.then((tabs) => {
     if (tabId == tabs[0].id) {
       restartAlarm(tabId);
@@ -51,7 +57,7 @@ function restartAlarm(tabId) {
   var gettingTab = browser.tabs.get(tabId);
   gettingTab.then((tab) => {
     if (tab.url != CATGIFS) {
-      browser.alarms.create("", {delayInMinutes: DELAY});
+      browser.alarms.create("", { delayInMinutes: DELAY });
     }
   });
 }
@@ -60,7 +66,10 @@ function restartAlarm(tabId) {
 On alarm, show the page action.
 */
 browser.alarms.onAlarm.addListener((alarm) => {
-  var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
+  var gettingActiveTab = browser.tabs.query({
+    active: true,
+    currentWindow: true,
+  });
   gettingActiveTab.then((tabs) => {
     browser.pageAction.show(tabs[0].id);
   });
@@ -70,38 +79,46 @@ browser.alarms.onAlarm.addListener((alarm) => {
 On page action click, navigate the corresponding tab to the cat gifs.
 */
 browser.pageAction.onClicked.addListener(() => {
-  browser.tabs.query({active:true,currentWindow:true}).then(function(tabs){
-    browser.tabs.executeScript(tabs[0].tabId, {file: "/content_scripts/send_canonical_url.js"});
-  });
+  browser.tabs
+    .query({ active: true, currentWindow: true })
+    .then(function (tabs) {
+      browser.tabs.executeScript(tabs[0].tabId, {
+        file: "/content_scripts/send_canonical_url.js",
+      });
+    });
 });
 
 browser.runtime.onMessage.addListener(recieveCanoUrl);
 
 function recieveCanoUrl(message) {
-  console.log("received: " + message.url)
+  console.log("received: " + message.url);
 
-  browser.tabs.query({active:true,currentWindow:true}).then(function(tabs){
-    const currentTabUrl = message.url ? message.url : tabs[0].url;
-    console.log("currentTabUrl" + currentTabUrl);
-    const hatenaUrl = "http://b.hatena.ne.jp/api/viewer.popular_bookmarks?url=" + encodeURIComponent(currentTabUrl);
-    console.log("hatenaUrl: " + hatenaUrl);
-    const req = new Request(hatenaUrl);
-    fetch(req).then(response => {
-      if(!response.ok) {
-        const status = response.status;
-        response.text().then(text => {
-          browser.notifications.create({
-            "type": "basic",
-            "title": "status: " + status,
-            "message": currentTabUrl + " " + text
+  browser.tabs
+    .query({ active: true, currentWindow: true })
+    .then(function (tabs) {
+      const currentTabUrl = message.url ? message.url : tabs[0].url;
+      console.log("currentTabUrl" + currentTabUrl);
+      const hatenaUrl =
+        "http://b.hatena.ne.jp/api/viewer.popular_bookmarks?url=" +
+        encodeURIComponent(currentTabUrl);
+      console.log("hatenaUrl: " + hatenaUrl);
+      const req = new Request(hatenaUrl);
+      fetch(req).then((response) => {
+        if (!response.ok) {
+          const status = response.status;
+          response.text().then((text) => {
+            browser.notifications.create({
+              type: "basic",
+              title: "status: " + status,
+              message: currentTabUrl + " " + text,
+            });
           });
+          return;
+        }
+        const json = response.json().then((data) => {
+          const entry_url = data.entry_url;
+          browser.tabs.update({ url: entry_url });
         });
-        return;
-      }
-      const json = response.json().then(data => {
-        const entry_url = data.entry_url;
-        browser.tabs.update({url: entry_url});
       });
     });
-  });
 }
